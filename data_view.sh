d94418ce2846b1f67dd1c7b0ece6897ef8505b7e
seu_lrx <17380708744@163.com>
2026-03-03 17:07:45 +0800

    补充提交：清理旧文件+新增测试文件

--- data_view.sh
+++
@@ -1,8 +0,0 @@
-# 运行view_BG.py
-#! /bin/bash
-SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
-# data_1011
-FILE_PATH="/data1/tonghua/BG/GB.2023050300"
-# data_0403
-FILE_PATH_1="/data1/lrx/data_0403/IC/IC.2021020112"
-python $SCRIPT_PATH/tools/view_BG.py --file_path $FILE_PATH_1
